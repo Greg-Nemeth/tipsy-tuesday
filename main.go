0e7b8e3890fe6edbb15cc2bd33d3628c1992ecc1
Greg Nemeth <gregmix88@gmail.com>
2021-12-18 17:42:03 +0000

    methods handled

--- main.go
+++ main.go
@@ -7,14 +7,29 @@ import (
 
 type server struct{}
 
-func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "Hello World"}`))
+	switch r.Method {
+	case "GET":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "GET called"}`))
+	case "POST":
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(`{"message": "POST called"}`))
+	case "PUT":
+		w.WriteHeader(http.StatusAccepted)
+		w.Write([]byte(`{"message": "PUT called"}`))
+	case "DELETE":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "DELETE called"}`))
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(`{"message": "Not Found"}`))
+	}
 }
 
 func main() {
-	s := &server{}
-	http.Handle("/", s)
+	http.HandleFunc("/", home)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
